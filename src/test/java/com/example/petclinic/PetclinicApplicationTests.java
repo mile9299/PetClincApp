390043180c6ef7ae013ea0d47b915c26aa916a88
Baha <baha@pop-os.localdomain>
2023-05-01 15:38:56 +0100

    updated tests

--- src/test/java/com/example/petclinic/PetclinicApplicationTests.java
+++ src/test/java/com/example/petclinic/PetclinicApplicationTests.java
@@ -2,10 +2,27 @@ package com.example.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class PetclinicApplicationTests {
 
+    @Container
+    public static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
+            .withPassword("postgres")
+            .withUsername("root");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+    }
     @Test
     void contextLoads() {
         System.out.println("hello petclinic !");
