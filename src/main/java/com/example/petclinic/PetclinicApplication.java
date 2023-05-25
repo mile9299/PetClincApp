c44d9b15b563474a9046345af4a201c5cc388d17
Baha <baha@pop-os.localdomain>
2023-05-25 09:54:31 +0100

    changed some code

--- src/main/java/com/example/petclinic/PetclinicApplication.java
+++ src/main/java/com/example/petclinic/PetclinicApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetclinicApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello from PPP!");
         SpringApplication.run(PetclinicApplication.class, args);
     }
 
